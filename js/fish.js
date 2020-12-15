fe0b4d56dd67e1fc257c37a00e34d53094b937ea
linux9420 <461766361@qq.com>
2020-12-15 15:54:15 +0800

    Site updated: 2020-12-15 15:54:15

--- js/fish.js
+++ js/fish.js
@@ -142,7 +142,7 @@ var RENDERER = {
 		requestAnimationFrame(this.render);
 		this.controlStatus();
 		this.context.clearRect(0, 0, this.width, this.height);
-		this.context.fillStyle = 'hsl( 113, 47%, 89%)';
+		this.context.fillStyle = 'hsl( 360, 96%, 71%)';
 		
 		for(var i = 0, count = this.fishes.length; i < count; i++){
 			this.fishes[i].render(this.context);
